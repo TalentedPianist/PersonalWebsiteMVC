82aaebd5dbf84f63720dcc9085f6d82cc6473c05
Douglas McGregor <douglasmcgregor@Douglass-MacBook-Air.local>
2021-04-09 01:35:57 +0100

    Changes 3 08/04/2021

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -2,23 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PersonalWebsiteMVC.Areas.Admin.Controllers
 {
-     [Area("Admin")]
-     [Authorize(Policy = "Admin")]
-     public class AdminController : Controller
-     {
-          public IActionResult Index()
-          {
-               return View();
-          }
+	[Area("Admin")]
+	public class AdminController : Controller
+	{
 
-          public IActionResult SignlR()
-          {
-               return View();
-          }
-     }
+
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
 }
