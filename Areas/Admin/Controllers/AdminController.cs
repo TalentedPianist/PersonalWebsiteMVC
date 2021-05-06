74da87dd428d9f6ec5b76b90dd59796edf15e94b
douglasmcgregor <douglas@douglasmcgregor.co.uk>
2021-05-06 22:16:00 +0100

    Changes 1 06.05.2021

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace PersonalWebsiteMVC.Areas.Admin.Controllers
 {
 	[Area("Admin")]
+	[Authorize(Policy ="Admin")]
 	public class AdminController : Controller
 	{
 
