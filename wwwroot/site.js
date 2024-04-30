d7d885b98a941aca0aa03d018a2ee8f99d5f80a3
Douglas McGregor <41552151+TalentedPianist@users.noreply.github.com>
2024-04-30 19:08:54 +0100

    Changes 1 30/04/2024

---
+++ wwwroot/site.js
@@ -0,0 +1,66 @@
+﻿
+function DetectScroll() {
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() >= 330) {
+            $("header").addClass("sticky");
+        } else {
+            $("header").removeClass("sticky");
+        }
+    });
+}
+
+function SmoothScroll(target) {
+    // This needs the target parameter to work, otherwise we get an error with top, because it can't find it.
+    if (target.length) {
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, target)).offset().top
+        }, 500);
+      
+    }
+
+}
+
+
+function HideIcon() {
+    $('.MenuIcon').css('display', 'none');
+}
+
+function ShowIcon() {
+    $('.MenuIcon').css('display', 'flex');
+}
+
+function LoadMore() {
+    $(function () {
+        // class .mud-paper seems to work here
+        $('.mud-paper').hide();
+        $('.mud-paper').slice(0, 3).show();
+        x = 3; 
+        $('.loadMorePosts').on('click', function (e) {
+            e.preventDefault();
+            x = x + 1;
+            $('.mud-paper').slice(0, x).show(); // show x amount of images
+        });
+    });
+}
+
+function HideContent() {
+    $("#MainContent").hide();
+}
+
+
+function LoadCKEditor() {
+    /*
+    ClassicEditor
+        .create(document.querySelector('#editor1'), {
+            toolbar: ['undo', 'redo', 'bold', 'italic', 'numberedList', 'bulletedList']
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    */
+   
+}
+
+
+
