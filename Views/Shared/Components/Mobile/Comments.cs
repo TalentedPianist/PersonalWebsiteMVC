ed4bdc69b72960057d6889fb0170977bd6b0dafa
Douglas McGregor <41552151+TalentedPianist@users.noreply.github.com>
2024-08-16 19:17:54 +0100

    Changes 1 16/08/2024

---
+++ Views/Shared/Components/Mobile/Comments.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebsiteMVC.Data;
+using PersonalWebsiteMVC.Models;
+
+namespace PersonalWebsiteMVC.Views.Shared.Components.Mobile
+{
+   [ViewComponent]
+    public class Comments : ViewComponent
+    {
+        private ApplicationDbContext _db { get; set; }
+        public Comments(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var model = new MixModel();
+            model.AllComments = _db.Comments.Where(c => c.PostID == id).ToList();
+            model.Comments = new Models.Comments();
+            return View("~/Views/Shared/Components/Mobile/Comments.cshtml", model);
+        }
+    }
+}
