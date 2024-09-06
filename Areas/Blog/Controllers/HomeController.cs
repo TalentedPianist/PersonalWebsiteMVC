059916df21f1d1519922fdf26eb56e2576bdc09d
Douglas McGregor <41552151+TalentedPianist@users.noreply.github.com>
2024-09-06 02:59:56 +0100

    Fixed null model problem.

--- Areas/Blog/Controllers/HomeController.cs
+++ Areas/Blog/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using PersonalWebsiteMVC.Models;
 using X.PagedList.Extensions;
 using DeviceDetectorNET;
 using DeviceDetectorNET.Parser;
+using X.PagedList;
 
 namespace PersonalWebsiteMVC.Areas.Blog.Controllers
 {
@@ -23,22 +24,25 @@ namespace PersonalWebsiteMVC.Areas.Blog.Controllers
         [Route("Areas/Blog/Views/Index")]
         public IActionResult Index()
         {
-            TempData["Message"] = DateTime.Now;
-            return View("~/Areas/Blog/Views/Index.cshtml");
+            // Spending ages editing the wrong function and wondering why nothing's happening is something I will need to watch.
+            BlogCommentViewModel model = new BlogCommentViewModel();
+            model.PagedPosts = _db.Posts.ToPagedList(1, 1);
+
+            return View("~/Areas/Blog/Views/Index.cshtml", model);
         }
 
         [Route("Blog/SinglePost/{id}")]
-        public IActionResult SinglePost(int id, [FromQuery(Name="pageNumber")]int? page)
+        public IActionResult SinglePost(int? id, [FromQuery(Name="pageNumber")]int? page)
         { // https://www.codeproject.com/Articles/1108855/10-Ways-to-Bind-Multiple-Models-on-a-View-in-MVC
             BlogCommentViewModel model = new BlogCommentViewModel();
             model.Post = _db.Posts.Where(p => p.PostID == id).FirstOrDefault();
             model.Comments = _db.Comments.Where(c => c.PostID == id).ToList();
-
-            var posts = _db.Posts;
-            var pageNumber = page ?? 1;
-            model.PagedPosts = (X.PagedList.PagedList<Posts>?)posts.ToPagedList(pageNumber, 1);
-          
-            ViewBag.PostID = id;
+            // Just spent hours editing the wrong function.  No wonder it's always null.  What an eejit.
+      
+            //var pageNumber = page ?? 1;
+            ViewBag.OnePageOfPosts = _db.Posts.ToPagedList(1, 1);
+            // The problem was in the model.  It needs to be IPagedList instead of PagedList.  This is the sort of thing you could waste so much time on.
+            model.PagedPosts = _db.Posts.ToPagedList<Posts>(1, 1);
             return View(model);
         }
 
