8b21b23fffb46fd90a2889b22f682ea14c3f21aa
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-05-28 05:33:26 +0100

    Implemented smooth scroll and sticky header!!!

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,24 @@
+document.querySelector("#About").on('click', function (event) {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    var hash = this.hash;
 
-window.HelloWorld = () => { 
-    console.log('Hello World!');
-}
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function () {
+        // Add hash (#) to URL when done scrolling (default click behaviour) 
+        window.location.hash = hash;
+    });
+});
+
+// Sticky header
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $('header').addClass('sticky');
+    } else {
+        $('header').removeClass('sticky');
+    }
+});
