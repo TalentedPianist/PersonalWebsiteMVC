1e8ce1947c2c0641949dad64e743d305dd3a8c46
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-05-30 04:06:49 +0100

    Changes 1 29/05/2025

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,24 +1,27 @@
-document.querySelector("#About").on('click', function (event) {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 800, function () {
-        // Add hash (#) to URL when done scrolling (default click behaviour) 
-        window.location.hash = hash;
-    });
-});
 
 // Sticky header
 $(window).scroll(function () {
-    if ($(this).scrollTop() > 1) {
+    console.log($(this).scrollTop());
+    if ($(this).scrollTop() > 100) {
         $('header').addClass('sticky');
+        $("header").prependTo("body");
     } else {
         $('header').removeClass('sticky');
+        $("header").prependTo(".overlay");
     }
 });
+
+window.ScrollToView = (name) => { 
+    $('html, body').animate({ 
+        scrollTop: $("#" + name).offset().top
+    }, 200);
+}
+
+window.scrollToHash = () => { 
+    var hash = window.location.hash;
+    if (hash && $(hash).length)  {
+        $('html,body').animate({ 
+            scrollTop: $(hash).offset().top
+        }, 900, 'swing');
+    }
+}
