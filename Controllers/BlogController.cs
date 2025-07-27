5c182861834263b4a03cbf36d47aa0eb471de135
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-07-27 10:47:57 +0100

    MixModel is finally working!!!!

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -7,7 +7,7 @@ using PersonalWebsiteMVC.Models;
 
 namespace PersonalWebsiteMVC.Controllers
 {
-    [Route("Blog")]
+
     public class BlogController : Controller
     {
         public ApplicationDbContext _db { get; set; }
@@ -16,13 +16,14 @@ namespace PersonalWebsiteMVC.Controllers
             _db = db;
         }
 
+
         public IActionResult Index()
         {
             // For the mobile view, the HTML needs to be hard coded in the partial view rather than the traditional MVC approach.  After playing around with ViewComponents and discovering that POST is disabled it makes sense.
 
             if (HttpContext.Request.Query["id"].ToString() != null)
             {
-                return PartialView("~/Views/Shared/Blog/SinglePost.cshtml");
+                return View("~/Views/Shared/Blog/SinglePost.cshtml");
             }
             else
             {
@@ -34,11 +35,12 @@ namespace PersonalWebsiteMVC.Controllers
         // https://dotnettutorials.net/lesson/partial-view-result-in-asp-net-core-mvc/?utm_content=cmp-true
         // Also in dotnet 8, asp-action won't seem to work without the Route attribute. 
         [HttpGet]
-        [Route("{title}")]
+        [Route("/Blog/{title}")]
         public IActionResult SinglePost(string title)
         {
             MixModel model = new MixModel();
             model.Posts = _db.Posts.Where(p => p.PostTitle == title).FirstOrDefault();
+            model.Comments = new Comments();
             model.AllComments = _db.Comments.Where(p => p.PostID == model.Posts!.PostID).ToList();
             return View("~/Views/Mobile/SinglePost.cshtml", model);
         }
@@ -46,7 +48,9 @@ namespace PersonalWebsiteMVC.Controllers
         public IActionResult Comments(int id)
         {
             TempData["Message"] = DateTime.Now;
-            return PartialView("~/Views/Blog/Comments.cshtml");
+            return View("~/Views/Blog/Comments.cshtml");
         }
+
+      
     }
 }
--- Controllers/MobileController.cs
+++ Controllers/MobileController.cs
@@ -17,31 +17,53 @@ public class MobileController : Controller
         _db = db;
     }
 
-    [HttpPost("/Blog/AddComment")]
-    public async Task<IActionResult> AddComment(Comments comment, [FromForm(Name="g-recaptcha-response")]string captchaResponse, [FromForm(Name="PostTitle")] string title, [FromForm(Name="PostID")]int id)
-    {
-        TempData["Message"] = await VerifyCaptcha(captchaResponse);
 
-        MixModel model = new MixModel();
+
+    public async Task<IActionResult> SubmitContactForm(ContactFormModel model)
+    {
+        TempData["Message"] = model.Name;
+        await Task.CompletedTask;
+        return View("~/Views/Mobile/Mobile.cshtml");
+    }
+
+    // https://www.c-sharpcorner.com/blogs/implementing-captcha-in-asp-net-core-web-application
+    [HttpPost]
+    [Route("/Blog/AddComment")]
+    public async Task<IActionResult> AddComment(MixModel model, [FromForm(Name = "PostTitle")] string title, [FromForm(Name = "PostID")] int id)
+    {
+        TempData["Message"] = model.Comments.CommentAuthor;
+
+
         model.Posts = _db.Posts.Where(p => p.PostTitle == title).FirstOrDefault();
+        model.Comments = new Comments();
         model.AllComments = _db.Comments.Where(p => p.PostID == id).ToList();
 
+        // if (!await VerifyCaptcha(captchaResponse))
+        // {
+        //     return View("~/Views/Mobile/SinglePost.cshtml", model);
+        // }
+        // else
+        // {
+        //     ModelState.AddModelError("", "Error validating reCaptcha.  Please try again.");
+        //     return View("~/Views/Mobile/SinglePost.cshtml", model);
+        // }
         await Task.CompletedTask;
         return View("~/Views/Mobile/SinglePost.cshtml", model);
     }
 
-    private async Task<string> VerifyCaptcha(string captchaResponse)
-    {
-        var secretKey = "6LeCBlUrAAAAACVipFQ2hXQkaRn1i_pFJEZIegge";
-        var httpClient = new HttpClient();
-        var response = await httpClient.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret={secretKey}&response={captchaResponse}");
-        if (response.IsSuccessStatusCode)
-        {
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            var captchaResult = JsonConvert.DeserializeObject<CaptchaResponse>(jsonResponse);
-            return jsonResponse;
-        }
-        return string.Empty;
-    }
+    // private async Task<bool> VerifyCaptcha(string captchaResponse)
+    // {
+    //     var secretKey = "6LeCBlUrAAAAACVipFQ2hXQkaRn1i_pFJEZIegge";
+    //     var httpClient = new HttpClient();
+    //     var response = await httpClient.GetAsync($"https://www.google.com/recaptcha/api/siteverify?secret={secretKey}&response={captchaResponse}");
+    //     if (response.IsSuccessStatusCode)
+    //     {
+    //         var jsonResponse = await response.Content.ReadAsStringAsync();
+    //         var captchaResult = JsonConvert.DeserializeObject<CaptchaResponse>(jsonResponse);
+    //         return captchaResult!.Success;
+
+    //     }
+    //     return false;
+    // }
 
 }
