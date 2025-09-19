dba468d5ea8dfbc7032e23960b8d0bb798d5cd50
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-09-19 20:29:46 +0100

    Changes 1 18/09/2025

--- Areas/Admin/Controllers/OneDriveController.cs
+++ Areas/Admin/Controllers/OneDriveController.cs
@@ -15,29 +15,7 @@ namespace PersonalWebsiteMVC.Areas.Admin.Controllers
           [Route("/Admin/OneDrive")]
           public IActionResult Index(Settings settings)
           {
-               var scopes = new[] { "https://graph.microsoft.com/.default" };
-
-               var clientId = "4cb4c3f8-b663-44bb-9f7c-ec3ee7e42edb";
-               var tenantId = "6e07ac96-b10b-43e3-b9a4-37d7dfcddfab";
-               var clientSecret = "2418Q~luZQBl7PxzwIq_Hnuz.nUlYtxScSc6daC-";
-
-               var credential = new DeviceCodeCredential(options =>
-               {
-                    options.ClientId = "your-client-id";
-                    options.TenantId = "your-tenant-id";
-                    options.DeviceCodeCallback = info =>
-                    {
-                         Console.WriteLine(info.Message); // Prompts user to sign in
-                         return Task.CompletedTask;
-                    };
-               });
-
-               var authProvider = new AzureIdentityAuthenticationProvider(credential, scopes);
-               var adapter = new BaseGraphRequestAdapter(authProvider);
-               var graphClient = new GraphClient(adapter);
-
-               var me = await graphClient.Me.GetAsync();
-
+          
 
                return View("~/Areas/Admin/Views/OneDrive/Index.cshtml");
           }
