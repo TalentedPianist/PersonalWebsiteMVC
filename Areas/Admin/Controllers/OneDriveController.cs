127c5ffdecce21b096e7867f2e154bd2335fd2b8
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-09-21 20:18:24 +0100

    Changes 1 21/09/2025

--- Areas/Admin/Controllers/OneDriveController.cs
+++ Areas/Admin/Controllers/OneDriveController.cs
@@ -4,6 +4,7 @@ using PersonalWebsiteMVC.Helpers;
 using Microsoft.Graph;
 using MyGraphSdk;
 using Microsoft.Kiota.Http.HttpClientLibrary;
+using System.Text;
 
 namespace PersonalWebsiteMVC.Areas.Admin.Controllers
 {
@@ -18,8 +19,13 @@ namespace PersonalWebsiteMVC.Areas.Admin.Controllers
 
                var credential = new DeviceCodeCredential();
                var graphClient = new GraphServiceClient(credential);
-               var me = graphClient.Me;
-               TempData["Message"] = me.GetAsync().Result!.DisplayName;
+               var drive = graphClient.Me.Drives.GetAsync().Result;
+               StringBuilder sb = new StringBuilder();
+               foreach (var item in drive)
+               {
+                    sb.Append(item);
+               }
+               TempData["Message"] = sb.ToString();
                return View("~/Areas/Admin/Views/OneDrive/Index.cshtml");
           }
 
