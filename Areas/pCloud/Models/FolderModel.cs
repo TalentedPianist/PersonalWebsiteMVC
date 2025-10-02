603c6ac0937177662f407cdd6385ff572c266b4f
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-10-02 18:33:57 +0100

    Changes 1 02/10/2025

---
+++ Areas/pCloud/Models/FolderModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonalWebsiteMVC.Areas.pCloud.Models
+{
+     public class FolderModel
+     {
+          public string? icon { get; set; }
+          public string? id { get; set; }
+          public string? modified { get; set; }
+          public string? path { get; set; }
+          public bool? thumb { get; set; }
+          public DateTime? created { get; set; }
+          public int folderid { get; set; }
+          public bool isshared { get; set; }
+          public bool isfolder { get; set; }
+          public bool ismine { get; set; }
+          public string? name { get; set; }
+     }
+}
