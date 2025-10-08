62598e61e8c2a1fab55e4afaf5b6c322b13651b4
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2025-10-08 07:30:52 +0100

    Changes 2 07/10/2025

--- Areas/pCloud/Controllers/PhotosController.cs
+++ Areas/pCloud/Controllers/PhotosController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using PersonalWebsiteMVC.Areas.pCloud.Models;
 using System.Text;
 using SharpCompress;
+using PersonalWebsiteMVC.Data;
 
 namespace PersonalWebsiteMVC.Areas.pCloud.Controllers
 {
@@ -14,11 +15,14 @@ namespace PersonalWebsiteMVC.Areas.pCloud.Controllers
      {
           public IHttpClientFactory _httpClientFactory { get; set; }
           public IHttpContextAccessor _http { get; set; }
+          public ApplicationDbContext _db { get; set; }
 
-          public PhotosController(IHttpClientFactory httpClientFactory,  IHttpContextAccessor http)
+
+          public PhotosController(IHttpClientFactory httpClientFactory,  IHttpContextAccessor http, ApplicationDbContext db)
           {
                _httpClientFactory = httpClientFactory;
                _http = http;
+               _db = db; 
           }
 
           [Route("/pCloud/Photos/{id}/{name}")]
@@ -29,5 +33,25 @@ namespace PersonalWebsiteMVC.Areas.pCloud.Controllers
                return View();
           }
 
+          [HttpGet]
+          [Route("/pCloud/Photos/IsInDb")]
+          public IActionResult IsInDb(string name)
+          {
+               var photo = _db.Photos.Where(p => p.Name == name).FirstOrDefault();
+               if (photo is not null)
+                    return Ok(true);
+               else
+                    return Ok(false);
+          }
+
+          [HttpPost]
+          [Route("/pCloud/photos/AddMultipleToDb")]
+          public IActionResult AddMultipleToDb([FromBody]List<PersonalWebsiteMVC.Models.Photos> data)
+          {
+               _db.Photos.AddRange(data);
+               _db.SaveChanges();
+               
+               return Ok(data);
+          }
      }
 }
