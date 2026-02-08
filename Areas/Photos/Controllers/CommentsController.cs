21265d633c21808a2cb9b800ac4fe185bb589a91
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2026-02-08 02:30:34 +0000

    Changes 1 07/02/2026

--- Areas/Photos/Controllers/CommentsController.cs
+++ Areas/Photos/Controllers/CommentsController.cs
@@ -1,32 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PersonalWebsiteMVC.Data;
+using PersonalWebsiteMVC.Models;
 
 namespace PersonalWebsiteMVC.Areas.Photos.Controllers
 {
-    public class CommentsController : Controller
-    {
-        public ApplicationDbContext _db;
-        public IHttpContextAccessor _http;
-        public CommentsController(ApplicationDbContext db, IHttpContextAccessor http)
-        {
-            _db = db;
-            _http = http;
-        }
+     public class CommentsController : Controller
+     {
+          public ApplicationDbContext _db;
+          public IHttpContextAccessor _http;
+          public CommentsController(ApplicationDbContext db, IHttpContextAccessor http)
+          {
+               _db = db;
+               _http = http;
+          }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+          public IActionResult Index()
+          {
+               return View();
+          }
 
-        [HttpPost]
-        [Route("GetComments")]
-        public IActionResult GetComments(int id)
-        {
-            var comments = _db.Comments.
-                Where(c => c.PostID == id)
-                .OrderByDescending(c => c.CommentDate)
-                .ToList();
-            return Ok(comments);
-        }
-    }
+          [HttpPost]
+          [Route("GetComments")]
+          public IActionResult GetComments(int id)
+          {
+               var comments = _db.Comments.
+                   Where(c => c.PostID == id)
+                   .OrderByDescending(c => c.CommentDate)
+                   .ToList();
+               return Ok(comments);
+          }
+
+          [HttpPost]
+          [Route("/Album/Comments/AddComment")]
+          public IActionResult AddComment(Comments model)
+          {
+               Comments comment = new Comments();
+               comment.PhotoID = model.PhotoID;
+               comment.CommentAuthor = model.CommentAuthor;
+               comment.CommentAuthorEmail = model.CommentAuthorEmail;
+               comment.CommentAuthorUrl = model.CommentAuthorUrl;
+               comment.CommentContent = model.CommentContent;
+               comment.CommentAuthorIP = HttpContext.Connection.RemoteIpAddress!.ToString();
+               comment.CommentDate = DateTime.Now;
+               _db.Comments.Add(comment);
+               _db.SaveChanges();
+               return Ok(comment);
+          }
+     }
 }
