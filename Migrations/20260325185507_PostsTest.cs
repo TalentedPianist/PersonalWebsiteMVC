8431c799cea055b522c34798f2f61f11e720bba2
Douglas McGregor <douglas@douglasmcgregor.co.uk>
2026-03-25 19:28:11 +0000

    Changes 1 25/03/2026

---
+++ Migrations/20260325185507_PostsTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalWebsiteMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostsTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
