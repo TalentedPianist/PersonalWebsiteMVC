1edf0073a9c72d84977dd9cc73b2c0a88b542805
Douglas McGregor <Douglas@douglasmcgregor.co.uk>
2026-06-09 06:42:19 +0100

    Changes 1 09/06/2026

--- Api/RolesController.cs
+++ Api/RolesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace PersonalWebsiteMVC.Api
 {
@@ -19,6 +21,52 @@ namespace PersonalWebsiteMVC.Api
           public IActionResult ListRoles()
           {
                return Ok(roleManager.Roles);
+          }
+
+          [HttpPost("/roles/Create")]
+          [IgnoreAntiforgeryToken]
+          public async Task<IActionResult> CreateRole([Required][FromForm(Name="name")] string name)
+          {
+               StringBuilder sb = new StringBuilder();
+               if (ModelState.IsValid)
+               {
+                    IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                    if (result.Succeeded)
+                         return Ok("Role created successfully");
+
+                    return Ok();
+               } else {
+                    return Problem("Please enter a role name.");
+               }
+               
+
+          }
+
+          [HttpGet("/roles/role/{id}")]
+          public async Task<IActionResult> GetRole(string id)
+          {
+               return Ok(await roleManager.FindByIdAsync(id));
+          }
+
+
+          [HttpPut("/roles/update/{id}")]
+          public async Task<IActionResult> UpdateRole(string id, [FromForm(Name="name")]string name)
+          {
+               if (string.IsNullOrWhiteSpace(name))
+               {
+                    return Problem("Please provide the role name.");
+               }
+               else
+               {
+                    var role = await roleManager.FindByIdAsync(id);
+                    if (role is not null)
+                    {
+                         role.Name = name;
+                         await roleManager.UpdateAsync(role);
+                         return Ok($"Role {name} updated successfully");
+                    }
+                    return NotFound(new { Message = "There was a problem" });
+               }
           }
 
      }
